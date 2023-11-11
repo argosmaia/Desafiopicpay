266d05ec8274580323c25ff8bcea30c77577cc57
Argos Maia <argosantao2013@gmail.com>
2023-11-11 04:39:19 -0300

    Uploading project

---
+++ src/main/java/com/picpaysimplificado/domain/user/UserType.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package com.picpaysimplificado.domain.user;
+
+/**
+ * 
+ */
+public enum UserType {
+	COMMON,
+	MERCHANT
+}
---
+++ src/test/java/com/picpaysimplificado/picpaysimplicado/PicpaysimplicadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.picpaysimplificado.picpaysimplicado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicpaysimplicadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
